c33ba689d63469582f40b92a13a2d1993a3765dd
Williamz21 <u201816224@upc.edu.pe>
2024-05-28 22:30:02 -0500

    feat: grade

---
+++ src/main/java/com/ttc/resource/admin/domain/persistence/GradeRepository.java
@@ -0,0 +1,22 @@
+package com.ttc.resource.admin.domain.persistence;
+
+import com.ttc.resource.admin.domain.model.entity.Grade;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+    @Query(value = "select g from Grade g " +
+            "where (g.name like concat('%', :filter, '%') or :filter is null) " +
+            "and (g.id = :id or :id is null)")
+    List<Grade> findByFilter(
+        @Param("filter") String filter,
+        @Param("id") Long id
+    );
+}
