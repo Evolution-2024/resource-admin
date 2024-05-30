0b14a8b30ff80e461c34c496a22e35e47605c96b
Yordy <84882709+Yordy-code81@users.noreply.github.com>
2024-05-30 18:46:24 -0500

    Update ResourceApplication.java

--- src/main/java/com/ttc/resource/ResourceApplication.java
+++ src/main/java/com/ttc/resource/ResourceApplication.java
@@ -3,7 +3,10 @@ package com.ttc.resource;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
@@ -13,5 +16,14 @@ public class ResourceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResourceApplication.class, args);
     }
-
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+          .allowedMethods("GET", "POST", "PUT", "DELETE");
+      }
+    };
+  }
 }
